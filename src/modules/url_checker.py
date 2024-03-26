bdf66daaaec276917f19af1b2d93d60c37a913fe
Brandon E <s4205051@glos.ac.uk>
2024-03-26 12:19:11 +0000

    updated URLs and url checker

---
+++ src/modules/url_checker.py
@@ -0,0 +1,21 @@
+def remove_duplicates(file_path):
+    unique_urls = set()
+    
+    # Read URLs from the file and add them to a set to remove duplicates
+    with open(file_path, "r") as file:
+        urls = file.readlines()
+        for url in urls:
+            unique_urls.add(url.strip())
+
+    # Write unique URLs back to the file
+    with open(file_path, "w") as file:
+        for url in unique_urls:
+            file.write(url + "\n")
+
+def main():
+    file_path = "././data/discovered_urls.txt"
+    remove_duplicates(file_path)
+    print("Duplicates removed from the file:", file_path)
+
+if __name__ == "__main__":
+    main()
