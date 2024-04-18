d0508f240b1845edd32bb754ea1570d0aa6b5f89
Talisman825 <114748904+s4205066@users.noreply.github.com>
2024-04-18 13:21:40 +0100

    Create streamlit.py

---
+++ streamlit.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from langchain.llms import OpenAI
+
+apiKey = st.sidebar.text_input('OpenAI API Key')
+
+def generate_response(prompt):
+  llm = OpenAI(temperature=0.5, openai_api_key=apiKey)
+  st.info(llm(prompt))
+
+with st.form('my_form'):
+  text = st.text_area('Enter text:', '')
+  submitted = st.form_submit_button('Submit')
+  if not apiKey.startswith('sk-'):
+    st.warning('Please enter your OpenAI API key!')
+  if submitted and apiKey.startswith('sk-'):
+    generate_response(text)
