ad8e4dcba12c283b0f1988503cb0c49c987ccae5
Usama Shahid <45976647+reachusama@users.noreply.github.com>
2024-04-18 12:22:33 +0100

    basic

--- src/pipeline/load.py
+++ src/pipeline/load.py
@@ -1,17 +1,27 @@
-from langchain_community.document_loaders import JSONLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
 from typing import (
     List,
     Iterable
 )
+import json
+
 
 
 def load_documents_from_json(path: str) -> List[Document]:
-    loader = JSONLoader(file_path=path)
-    docs = loader.load()
-    return docs
-
+    documents = []
+    
+    # Load JSON file
+    with open(path, 'r') as file:
+        data = json.load(file)
+    
+    # Iterate through each item in the JSON array
+    for item in data:
+        page_content = item["page_content"]
+        metadata = item["metadata"]
+        documents.append(Document(page_content=page_content, metadata=metadata))
+    
+    return documents
 
 def split_documents(
         documents: Iterable[Document],
@@ -35,5 +45,5 @@ def load(path: str) -> List[Document]:
 
 
 if __name__ == "__main__":
-    document_chunks = load("../data/training/input.json")
+    document_chunks = load("../../data/training/input.json")
     print(document_chunks[:2])
