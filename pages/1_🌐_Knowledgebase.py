51c9cef25d019a75bdef999ca64945e07cf3f499
Usama Shahid <45976647+reachusama@users.noreply.github.com>
2024-04-25 10:59:05 +0100

    added pages

--- pages/1_🌐_Knowledgebase.py
+++ pages/1_🌐_Knowledgebase.py
@@ -4,12 +4,12 @@ import pandas as pd
 st.set_page_config(page_title="Knowledgebase", page_icon="📈", layout="wide")
 
 st.markdown("# Knowledgebase")
-st.sidebar.header("Plotting Demo")
 st.write(
     """This demo illustrates a combination of plotting and animation with
 Streamlit. We're generating a bunch of random numbers in a loop for around
 5 seconds. Enjoy!"""
 )
+
 
 df = pd.read_json("data/training/input.json")
 df['metadata'] = df['metadata'].apply(lambda x: x['source'])
