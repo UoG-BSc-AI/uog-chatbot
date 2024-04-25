2b948c2173143217aadf5114ed96a1853b8d23f4
Usama Shahid <45976647+reachusama@users.noreply.github.com>
2024-04-25 10:59:32 +0100

    Update 1_🌐_Knowledgebase.py

--- pages/1_🌐_Knowledgebase.py
+++ pages/1_🌐_Knowledgebase.py
@@ -15,4 +15,4 @@ df = pd.read_json("data/training/input.json")
 df['metadata'] = df['metadata'].apply(lambda x: x['source'])
 df['page_content'] = df['page_content'].apply(lambda x: x.split("»")[-1])
 
-st.dataframe(df, use_container_width=True, hide_index=True, height=1000)
+st.dataframe(df, use_container_width=True, hide_index=True, height=500)
