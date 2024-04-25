163c69109d9ef61965962504e712d7e47f087fbb
Usama Shahid <45976647+reachusama@users.noreply.github.com>
2024-04-25 11:18:59 +0100

    Update 🎓_University_of_Gloucestershire_Chatbot.py

--- 🎓_University_of_Gloucestershire_Chatbot.py
+++ 🎓_University_of_Gloucestershire_Chatbot.py
@@ -13,6 +13,7 @@ apiKey = st.sidebar.text_input('OpenAI API Key')
 if not apiKey.startswith('sk-'):
     st.sidebar.warning('Please enter your OpenAI API key!')
 
+
 def generate_response(prompt):
     llm = OpenAI(temperature=0.5, openai_api_key=apiKey)
     st.info(llm(prompt))
@@ -26,6 +27,7 @@ def main():
     st.title('University of Gloucestershire Playground')
 
     with st.form('my_form'):
+
         text = st.text_area('Enter text:', '')
         submitted = st.form_submit_button('Submit')
 
