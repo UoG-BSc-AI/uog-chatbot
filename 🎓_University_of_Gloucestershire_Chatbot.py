b08ae0f3faede31c1ed2f1a1d845ecb3d5fbb542
robbiesmith11 <114229565+robbiesmith11@users.noreply.github.com>
2024-04-26 02:20:33 +0100

    UI Update
    Allowing users to pick from OpenAI model if they have a key and Ollama . My keys arent working if someone could take a look please

--- 🎓_University_of_Gloucestershire_Chatbot.py
+++ 🎓_University_of_Gloucestershire_Chatbot.py
@@ -3,6 +3,11 @@ from langchain_community.llms import OpenAI
 from langchain_community.llms import Ollama
 from src.pipeline.inference import setup
 
+
+#Implemented choice between Ollama and OpenAI models. (uses ollama if no key is entered)
+#Im not sure why but my openAI keys dont seem to be working, this may because of my account setup
+#If someone could test this it would be great. - Robbie
+
 st.set_page_config(
     page_title="University of Gloucestershire Chatbot",
     page_icon="🎓",
@@ -13,31 +18,42 @@ apiKey = st.sidebar.text_input('OpenAI API Key')
 if not apiKey.startswith('sk-'):
     st.sidebar.warning('Please enter your OpenAI API key!')
 
+def generate_response_openai(prompt):
 
-def generate_response(prompt):
-    llm = OpenAI(temperature=0.5, openai_api_key=apiKey)
-    st.info(llm(prompt))
+    if not apiKey.startswith('sk-'):
+        return "Please enter your OpenAI API key!"
+    try:
+        llm = OpenAI(temperature=0.5, openai_api_key=apiKey)
+        response = llm(prompt)
+        return response 
+    except Exception as e:
+        return f"Invalid Key"
+
+def generate_response_ollama(prompt):
+    llm = Ollama(model="stable-beluga", temperature=0)
+    chain = setup(docs_path="data/training/input.json", llm=llm)
+    response = chain({"query": prompt})
+    return response["result"]
 
 
 def main():
-    llm = Ollama(model="stable-beluga", temperature=0)
-    chain = setup(docs_path="data/training/input.json", llm=llm)
-    # q = "Describe your purpose and background knowledge"
-
     st.title('University of Gloucestershire Playground')
+
+    response = "Enter Your query here"
 
     with st.form('my_form'):
 
         text = st.text_area('Enter text:', '')
         submitted = st.form_submit_button('Submit')
 
-        if submitted and apiKey.startswith('sk-'):
-            response = chain({"query": text})
-            st.info(response['result'])
+        if submitted:
+            if apiKey.startswith('sk-'):
+                response = generate_response_openai(text)
 
-        elif submitted:
-            response = chain({"query": text})
-            st.info(response['result'])
+            else:
+                response = generate_response_ollama(text)
+            
+        st.info(response)
 
 
 if __name__ == "__main__":
