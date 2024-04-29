475c901621127271cad93292ad0c07cd398194b2
Usama Shahid <45976647+reachusama@users.noreply.github.com>
2024-04-29 16:33:09 +0100

    added stable beluga experiments

--- pages/3_🧪_Experiments.py
+++ pages/3_🧪_Experiments.py
@@ -12,7 +12,11 @@ df = pd.read_csv("data/experiments/experiments_queries.csv")
 st.dataframe(df[["Category", "Query"]], hide_index=True)
 
 st.write("""## [Llama3](https://ollama.com/library/llama3)""")
-st.dataframe(df[["Category", "Query"]], hide_index=True)
+df = pd.read_csv("data/experiments/experiments_ollama_stable_beluga.csv")
+df = df.rename(columns={"response": "Model response"})
+st.dataframe(df[["Category", "Query", "Model response"]], hide_index=True)
 
 st.write("""## [Stable-beluga](https://ollama.com/library/stable-beluga)""")
-st.dataframe(df[["Category", "Query"]], hide_index=True)
+df = pd.read_csv("data/experiments/experiments_ollama_stable_beluga.csv")
+df = df.rename(columns={"response": "Model response"})
+st.dataframe(df[["Category", "Query", "Model response"]], hide_index=True)
