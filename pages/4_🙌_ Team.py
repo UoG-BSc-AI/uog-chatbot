48559d733100bf2fff16820bd69e221023b00cd4
robbiesmith11 <114229565+robbiesmith11@users.noreply.github.com>
2024-05-04 15:46:25 +0100

    Update 4_🙌_ Team.py

--- pages/4_🙌_ Team.py
+++ pages/4_🙌_ Team.py
@@ -3,11 +3,11 @@ import pandas as pd
 
 st.set_page_config(page_title="Team", page_icon="🙌", layout="wide")
 
-st.markdown("# Team")
+st.markdown("UI and Deployement Team")
 st.write("BSc AI Level 5, University of Gloucestershire")
 
 st.markdown("## Data")
-st.write("- Name 1, One Line Contribution")
+st.write("- Robbie Smith, Building UI interface and intergrating models to UI for deployment")
 
 st.markdown("## Product")
 st.write("- Name 2, One Line Contribution")
