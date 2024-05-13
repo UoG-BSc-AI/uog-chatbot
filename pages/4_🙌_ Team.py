52d901d80f4f0209f3750c2ba46c1feec1a3b102
Brandon E <s4205051@glos.ac.uk>
2024-05-13 11:44:30 +0100

    Added contribution

--- pages/4_🙌_ Team.py
+++ pages/4_🙌_ Team.py
@@ -9,8 +9,11 @@ st.write("BSc AI Level 5, University of Gloucestershire")
 st.markdown("## Data")
 st.write("- Robbie Smith, Building UI interface and intergrating models to UI for deployment")
 
+st.markdown("## Data")
+st.write("- Brandon Elward, Building knowledge base for models to train from the data colected")
+
 st.markdown("## Product")
-st.write("- Lilia Vladova, Team Lead of UI and Developlemt")
+st.write("- Lilia Vladova, Team Lead of UI and Development")
 
 st.markdown("## Documentation")
 st.write("- Name 3, One Line Contribution")
